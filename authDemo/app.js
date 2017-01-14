0cc3ee5db0b016ecfa18fb7dce666a09e516610c
konrad <konrad@ugs.com>
2017-01-14 06:54:43 +0000

    upload added

--- authDemo/app.js
+++ authDemo/app.js
@@ -4,13 +4,19 @@ var     express      =require ("express"),
         bodyParser   =require("body-parser"),
         User         =require("./models/user"),
         LocalStrategy=require("passport-local"),
+        path = require('path'),
+        formidable = require('formidable'),
+        fs = require('fs'),
         passportLocalMongoose= require("passport-local-mongoose");
 
 mongoose.connect("mongodb://localhost/auth_demo_app");
 
+
+
 var app = express();
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(require("express-session")({
     secret:"Konrad is the best",
@@ -25,12 +31,13 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
+
 app.get("/", function(req,res){
     res.render("home");
 });
 
 app.get("/secret",isLoggedIn,function(req,res){
-    res.render("secret");
+    res.sendFile(path.join(__dirname, 'views/index.html'));
 });
 
 app.get("/register" ,function(req,res){
@@ -56,6 +63,10 @@ app.get("/login" ,function(req,res){
 });
 
 
+app.get("/uploads" ,function(req,res){
+    res.render("show");
+});
+
 app.post("/login" , passport.authenticate("local", {
     successRedirect: "/secret",
     failureRedirect: "/login"
@@ -74,6 +85,42 @@ function isLoggedIn(req, res, next){
     }
     res.redirect("/login")
 }
+
+
+
+app.post('/upload', function(req, res){
+
+  // create an incoming form object
+  var form = new formidable.IncomingForm();
+
+  // specify that we want to allow the user to upload multiple files in a single request
+  form.multiples = true;
+
+  // store all uploads in the /uploads directory
+  form.uploadDir = path.join(__dirname, '/uploads');
+
+  // every time a file has been uploaded successfully,
+  // rename it to it's orignal name
+  form.on('file', function(field, file) {
+    fs.rename(file.path, path.join(form.uploadDir, file.name));
+  });
+
+  // log any errors that occur
+  form.on('error', function(err) {
+    console.log('An error has occured: \n' + err);
+  });
+
+  // once all the files have been uploaded, send a response to the client
+  form.on('end', function() {
+    res.end('success');
+  });
+
+  // parse the incoming request containing the form data
+  form.parse(req);
+
+});
+
+
 
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("server started....");
